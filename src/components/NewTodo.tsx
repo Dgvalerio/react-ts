7d2cfdb550850e9e054b27ba087c7cc17b968bcc
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-26 15:24:52 -0300

    Class 409 - Working with refs & useRef

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,15 +1,23 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { FC, FormEvent } from 'react';
+import React, { FC, FormEvent, useRef } from 'react';
 
 const NewTodo: FC = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: FormEvent) => {
     event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw an error
+    }
   };
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo text</label>
-      <input id="text" type="text" />
+      <input id="text" type="text" ref={todoTextInputRef} />
       <button type="submit">Add Todo</button>
     </form>
   );
